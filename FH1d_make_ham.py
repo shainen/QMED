593a64e9d0f3a8baa745cdbb4cad336a08d020e8
Shainen Davidson <shainen@gmail.com>
2017-01-13 15:55:03 -0500

    nn 1d

--- FH1d_make_ham.py
+++ FH1d_make_ham.py
@@ -7,7 +7,7 @@ LENGTH = 16
 num_up_ferms = 4
 #initial_fermion_up = [0,0,0,0,1,1,0,0,0,0]
 #initial_fermion_up = [0,1,1,0]
-initial_fermion_up = [0 for _ in range(LENGTH)]
+#initial_fermion_up = [0 for _ in range(LENGTH)]
 #initial_fermion_up[LENGTH//2] = 1
 #for i in range(num_up_ferms//2): initial_fermion_up[LENGTH//2-1-i] = initial_fermion_up[LENGTH//2+i] = 1
 initial_fermion_up = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0]
@@ -22,13 +22,13 @@ DIMENSION = int(fermion_comb(num_up_ferms) * fermion_comb(num_up_ferms))
 
 print(DIMENSION)
 
-state_dict, [hop_mat] = b.make_matrices_and_states(initial_state, DIMENSION, b.hop_lr_a1_ob)
+state_dict, [hop_mat] = b.make_matrices_and_states(initial_state, DIMENSION, b.hop_nn_ob)
 
 import pickle
-pickle.dump( state_dict, open( "s16_FH_lr_statedict.p", "wb" ) )
-b.save_sparse_csr("l16_FH_lr_hop",hop_mat)
+pickle.dump( state_dict, open( "s16_FH_nn_statedict.p", "wb" ) )
+b.save_sparse_csr("l16_FH_nn_hop",hop_mat)
 
 int_mat = b.make_diag_ops(state_dict, lambda x: np.dot(x['up'],x['down']))
 
-b.save_sparse_csr("l16_FH_lr_int",int_mat)
+b.save_sparse_csr("l16_FH_nn_int",int_mat)
 
