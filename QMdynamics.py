f438d002893fb5614676790cce40e3ebd93d5981
Shainen Davidson <shainen@gmail.com>
2017-01-16 09:06:25 -0500

    dyn

--- QMdynamics.py
+++ QMdynamics.py
@@ -2,12 +2,12 @@ import ed_build_hil as b
 import numpy as np
 import pickle
 
-data_dir="/data/shainen/170110_9_s16_FH2d_nn_make_mats/"
+data_dir="/data/shainen/170113_5_s16_FH_nn_make_mats/"
 #data_dir=""
 
-hop_mat=b.load_sparse_csr(data_dir+"l16_FH2d_nn_hop.npz")
-int_mat=b.load_sparse_csr(data_dir+"l16_FH2d_nn_int.npz")
-state_dict = pickle.load( open( data_dir+"s16_FH2d_nn_statedict.p", "rb" ) )
+hop_mat=b.load_sparse_csr(data_dir+"l16_FH_nn_hop.npz")
+int_mat=b.load_sparse_csr(data_dir+"l16_FH_nn_int.npz")
+state_dict = pickle.load( open( data_dir+"s16_FH_nn_statedict.p", "rb" ) )
 
 U = 0
 
@@ -35,4 +35,4 @@ init_vec[0] = 1
 
 num_up_t = b.diag_ops_dynamics(init_vec, ham, TSTEPS, DT, num_up_diag)
 
-np.savetxt("s"+str(LENGTH)+"_FH2d_nn_u"+str(U)+".dat",num_up_t)
+np.savetxt("s"+str(LENGTH)+"_FH_nn_u"+str(U)+".dat",num_up_t)
