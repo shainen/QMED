b83f087994ee7b17a297fd2e2d529c3e360ddba0
Shainen Davidson <shainen@gmail.com>
2017-03-17 08:42:48 -0500

    syk half

--- SYK_make_ham.py
+++ SYK_make_ham.py
@@ -10,8 +10,8 @@ import numpy as np
 #initial_fermion_up = [0 for _ in range(LENGTH)]
 #initial_fermion_up[LENGTH//2] = 1
 #for i in range(num_up_ferms//2): initial_fermion_up[LENGTH//2-1-i] = initial_fermion_up[LENGTH//2+i] = 1
-#initial_fermion_up = [1 for _ in range(6)] + [0 for _ in range(3)]
-initial_fermion_up = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0]
+initial_fermion_up = [1 for _ in range(10)] + [0 for _ in range(10)]
+#initial_fermion_up = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0]
 initial_state = b.State({'up': initial_fermion_up})
 
 print(str(initial_state))
@@ -25,45 +25,45 @@ DIMENSION = int(fermion_comb(num_up_ferms))
 
 print(DIMENSION)
 
-# sigma = 1
+sigma = 1
 
-# J_real = np.random.normal(0,sigma,(SITES,SITES,SITES,SITES))
-# J_real = J_real - np.transpose(J_real,(1,0,2,3))
-# J_real = J_real - np.transpose(J_real,(0,1,3,2))
-# J_real = J_real + np.transpose(J_real,(2,3,0,1))
+J_real = np.random.normal(0,sigma,(SITES,SITES,SITES,SITES))
+J_real = J_real - np.transpose(J_real,(1,0,2,3))
+J_real = J_real - np.transpose(J_real,(0,1,3,2))
+J_real = J_real + np.transpose(J_real,(2,3,0,1))
 
-# J_imag = np.random.normal(0,sigma,(SITES,SITES,SITES,SITES))
-# J_imag = J_imag - np.transpose(J_imag,(1,0,2,3))
-# J_imag = J_imag - np.transpose(J_imag,(0,1,3,2))
-# J_imag = J_imag - np.transpose(J_imag,(2,3,0,1))
+J_imag = np.random.normal(0,sigma,(SITES,SITES,SITES,SITES))
+J_imag = J_imag - np.transpose(J_imag,(1,0,2,3))
+J_imag = J_imag - np.transpose(J_imag,(0,1,3,2))
+J_imag = J_imag - np.transpose(J_imag,(2,3,0,1))
 
-# J_coup = J_real + 1j*J_imag
+J_coup = J_real + 1j*J_imag
 
-# reflat=J_real.reshape(1,J_real.size)
-# imflat=J_imag.reshape(1,J_imag.size)
-# np.savetxt("flatJreal.CSV",reflat,delimiter=',')
-# np.savetxt("flatJimag.CSV",imflat,delimiter=',')
+reflat=J_real.reshape(1,J_real.size)
+imflat=J_imag.reshape(1,J_imag.size)
+np.savetxt("flatJreal.CSV",reflat,delimiter=',')
+np.savetxt("flatJimag.CSV",imflat,delimiter=',')
 
-datadir ='/projectnb/twambl/170207_1_s16f4/'
+# datadir ='/projectnb/twambl/170207_1_s16f4/'   
 
-J_real = np.loadtxt(datadir+"flatJreal.CSV",delimiter=',')
-J_imag = np.loadtxt(datadir+"flatJimag.CSV",delimiter=',')
-J_coup = (J_real + 1j*J_imag).reshape(SITES,SITES,SITES,SITES)
+# J_real = np.loadtxt(datadir+"flatJreal.CSV",delimiter=',')
+# J_imag = np.loadtxt(datadir+"flatJimag.CSV",delimiter=',')
+# J_coup = (J_real + 1j*J_imag).reshape(SITES,SITES,SITES,SITES)
 
 #print(J_coup)
 
 def SYKint(state, state_dict, next_states, build_mat):
     return b.SYK_model(state, state_dict, next_states, build_mat, J_coup)
 
-#funcs = [SYKint, b.correlation(1,2)]
-funcs = [SYKint] + [b.correlation(i,j) for i in range(SITES) for j in range(SITES)]
+funcs = [SYKint, b.correlation(1,2)]
+#funcs = [SYKint] + [b.correlation(i,j) for i in range(SITES) for j in range(SITES)]
 
 state_dict, mats = b.make_matrices_and_states(initial_state, DIMENSION, *funcs)
 
 import pickle
 pickle.dump( state_dict, open( "s"+str(SITES)+"SYK_statedict.p", "wb" ) )
 b.save_sparse_csr("s"+str(SITES)+"_SYK_mat",mats[0])
-for i in range(SITES):
-    for j in range(SITES):
-        b.save_sparse_csr("s"+str(SITES)+"_corr"+str(i)+str(j),mats[SITES*i+j+1])
+# for i in range(SITES):
+#     for j in range(SITES):
+#         b.save_sparse_csr("s"+str(SITES)+"_corr"+str(i)+str(j),mats[SITES*i+j+1])
 
