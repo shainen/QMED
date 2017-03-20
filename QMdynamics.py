498f908af3bcbada26d861c8428d1f73c9f6ef82
Shainen Davidson <shainen@gmail.com>
2017-03-20 09:22:52 -0400

    run

--- QMdynamics.py
+++ QMdynamics.py
@@ -2,27 +2,27 @@ import ed_build_hil as b
 import numpy as np
 import pickle
 
-data_dir="/data/shainen/170103_4_s16_FH_lr_make_mats/"
+#data_dir="/data/shainen/170103_4_s16_FH_lr_make_mats/"
 #data_dir="/Users/shainen/Dropbox/Research/fTWA/fermion velocity/QMED/"
-
+data_dir="/projectnb/twambl/170317_2_s16f8/"
 LENGTH = 16
 
-hop_mat=b.load_sparse_csr(data_dir+"l16_FH_lr_hop.npz")
-int_mat=b.load_sparse_csr(data_dir+"l16_FH_lr_int.npz")
-state_dict = pickle.load( open( data_dir+"s16_FH_lr_statedict.p", "rb" ) )
+# hop_mat=b.load_sparse_csr(data_dir+"l16_FH_lr_hop.npz")
+# int_mat=b.load_sparse_csr(data_dir+"l16_FH_lr_int.npz")
+# state_dict = pickle.load( open( data_dir+"s16_FH_lr_statedict.p", "rb" ) )
 
-#ham=b.load_sparse_csr(data_dir+"s"+str(LENGTH)+"_SYK_mat.npz")
+ham=b.load_sparse_csr(data_dir+"s"+str(LENGTH)+"_SYK_mat.npz")
 #corr_mats = [b.load_sparse_csr(data_dir+"s"+str(LENGTH)+"_corr"+str(i)+str(j)+".npz") for i in range(LENGTH) for j in range(LENGTH)]
 #int_mat=b.load_sparse_csr(data_dir+"l16_FH_nn_int.npz")
-#state_dict = pickle.load( open( data_dir+"s"+str(LENGTH)+"SYK_statedict.p", "rb" ) )
+state_dict = pickle.load( open( data_dir+"s"+str(LENGTH)+"SYK_statedict.p", "rb" ) )
 
-U = 2
+#U = 2
 
-ham = - hop_mat + U*int_mat
+#ham = - hop_mat + U*int_mat
 
 DIMENSION = ham.shape[0]
 
-TMAX = 10
+TMAX = 0.01
 TSTEPS = 100
 DT = TMAX/TSTEPS
 times = np.linspace(0,TMAX,TSTEPS,endpoint=False)
@@ -32,9 +32,9 @@ for key, val in state_dict.items():
     num_up_diag[val] = np.array(eval(key)['up'][:])
 num_up_diag = num_up_diag.transpose()
 
-hop_mat = None
-int_mat = None
-state_dict = None
+# hop_mat = None
+# int_mat = None
+# state_dict = None
 
 init_vec = np.zeros(DIMENSION)
 init_vec[0] = 1
@@ -42,7 +42,7 @@ init_vec[0] = 1
 num_up_t = b.diag_ops_dynamics(init_vec, ham, TSTEPS, DT, num_up_diag)
 #num_up_t , exp_corrs = b.both_ops_dynamics(init_vec, ham, TSTEPS, DT, num_up_diag, corr_mats)
 
-np.savetxt("s"+str(LENGTH)+"_FH_lr_u"+str(U)+".dat",num_up_t)
-#np.savetxt("s"+str(LENGTH)+"_SYK.dat",num_up_t)
+#np.savetxt("s"+str(LENGTH)+"_FH_lr_u"+str(U)+".dat",num_up_t)
+np.savetxt("s"+str(LENGTH)+"_SYK.dat",num_up_t)
 #np.savetxt("s"+str(LENGTH)+"_re_corrs.dat",np.real(exp_corrs))
 #np.savetxt("s"+str(LENGTH)+"_im_corrs.dat",np.imag(exp_corrs))
